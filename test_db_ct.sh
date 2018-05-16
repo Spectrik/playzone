2b6a4e5f3946acc203ec5a5927fa19178a0b1660
Spectrik <Spectrik@users.noreply.github.com>
2018-05-16 16:25:26 +0200

    Fix bugs #1

--- test_db_ct.sh
+++ test_db_ct.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Author: ojanas@redhat.com
+
+# TODO: Desc
+# TODO: get rid of jq with docker inspect --format
+
 # Requires systemd based system
 # Requires RPM based package manager
 # Requires mysql-client
 # Requires jq
-
-# TODO: Function if provided datadir is not already mounted in some other container
 
 ### Vars (default values)
 
@@ -22,7 +25,7 @@ ADMINER_LOCAL_PORT="8080"
 # Optional database dump file to restore
 DUMP_FILE=""
 
-# Name of the container
+# Name of the DB container
 CT_NAME=""
 
 # Mariadb docker image to use
@@ -39,30 +42,6 @@ MYSQL_ROOT_PASS="123456"
 
 ### Functions
 
-# Check if provided datadir is not already mounted in some other container
-check_datadir()
-{
-    local ID
-    local TMP
-
-    for ID in $(docker ps -a | awk '{print $1}' | tail -n +2)
-    do
-        # Get the mount paths from the container
-        TMP=$(docker inspect $ID | jq '.[0].Mounts' | grep "Source")
-
-        # Skip the empty ones
-        if [ -z "$TMP" ]; then
-            continue
-        fi
-
-        # Tell the user
-        if echo "$TMP" | grep -q "$DB_LOCAL_DATADIR"; then
-            echo "The local mount $DB_LOCAL_DATADIR is probably already mounted in container $ID"
-            exit 1
-        fi
-    done
-}
-
 # Show usage
 show_help ()
 {
@@ -73,29 +52,9 @@ show_help ()
     echo -e "\t\t--ctname       Name of the container"
     echo -e "\t\t--datadir      Local datadir folder to map to the one inside the CT (default \$HOME/mysql)"
     echo -e "\t\t--password     Mysql root password (default 123456)"
-    echo -e "\t\t--adminer      Run a web interface container to manage the database"
+    echo -e "\t\t--adminer      Deploy a web interface container to manage the database"
     echo -e "\t\t--adminerport  Local port to map to the adminer port inside the CT (default 8080)"
     echo -e "\t\t--getip        Get local IP of the container based on its name in order to log into DB via adminer"  
-}
-
-# Check if container is running
-is_running ()
-{
-    if docker ps -a | grep "$1" > /dev/null; then
-        echo "There is already some container with name $1!"
-        return 0
-    fi
-
-    return 1
-}
-
-# Check if package is istalled
-package_exists ()
-{
-    if ! rpm -qa "$1" > /dev/null 2>&1; then
-        echo "$1 is probably not installed! Install it first, please."
-        exit 1
-    fi
 }
 
 # Get local IP of CT
@@ -118,30 +77,8 @@ get_ip ()
     fi
 }
 
-# Check if docker image exists locally
-image_exists ()
-{
-    if ! docker images | awk '{print $1}' | tail -n +2 | grep "$1" > /dev/null; then
-        echo "Could not find the database image! Trying to pull it now..."
-
-        if ! docker pull "$1"; then
-            echo "Could not pull the image $1"
-            exit 1
-        fi
-    fi
-}
-
-# Check if port is already taken
-port_taken ()
-{
-    if netstat -natp | grep docker-proxy | grep "$1" > /dev/null; then
-        echo "The port $1 is already being used by other container!"
-        exit 1
-    fi
-}
-
-# Checks
-run_checks ()
+# Check functions
+general_checks ()
 {
     # Check if root
     if ! [ $(id -u) -eq 0 ]; then
@@ -168,10 +105,10 @@ run_checks ()
 
         exit 1
     fi
+}
 
-    # Get IP
-    get_ip
-
+db_checks ()
+{
     # Check if the local datadir mount exists
     if [ -n "$DB_LOCAL_DATADIR" ]; then
         if ! [ -e "$DB_LOCAL_DATADIR" ]; then
@@ -182,7 +119,7 @@ run_checks ()
 
     # Check if the DB dump file exists
     if [ -n "$DUMP_FILE" ]; then
-        if [ -e "$DUMP_FILE" ]; then
+        if ! [ -e "$DUMP_FILE" ]; then
             echo "The DB dump file $DUMP_FILE does not exist!"
             exit 1
         fi
@@ -190,7 +127,7 @@ run_checks ()
 
     # Check if the user provided  the name of the container
     if [ -z "$CT_NAME" ]; then
-        echo "You need to provide the name of the container!"
+        echo "You need to provide the name of the DB container in order to deploy it!"
         exit 1
     fi
 
@@ -202,6 +139,77 @@ run_checks ()
 
     # Check datadir
     check_datadir
+
+    # DB CT already existing?
+    if is_running "$CT_NAME"; then
+        exit 1
+    fi
+}
+
+# Check if provided datadir is not already mounted in some other container
+check_datadir()
+{
+    local ID
+    local TMP
+
+    for ID in $(docker ps -a | awk '{print $1}' | tail -n +2)
+    do
+        # Get the mount paths from the container
+        TMP=$(docker inspect $ID | jq '.[0].Mounts' | grep "Source")
+
+        # Skip the empty ones
+        if [ -z "$TMP" ]; then
+            continue
+        fi
+
+        # Tell the user
+        if echo "$TMP" | grep -q "$DB_LOCAL_DATADIR"; then
+            echo "The local mount $DB_LOCAL_DATADIR is probably already mounted in container $ID"
+            exit 1
+        fi
+    done
+}
+
+# Check if container is running
+is_running ()
+{
+    if docker ps -a | grep "$1" > /dev/null; then
+        echo "There is already some container with name $1!"
+        return 0
+    fi
+
+    return 1
+}
+
+# Check if package is istalled
+package_exists ()
+{
+    if ! rpm -qa "$1" > /dev/null 2>&1; then
+        echo "$1 is probably not installed! Install it first, please."
+        exit 1
+    fi
+}
+
+# Check if docker image exists locally
+image_exists ()
+{
+    if ! docker images | awk '{print $1}' | tail -n +2 | grep "$1" > /dev/null; then
+        echo "Could not find the database image! Trying to pull it now..."
+
+        if ! docker pull "$1"; then
+            echo "Could not pull the image $1"
+            exit 1
+        fi
+    fi
+}
+
+# Check if port is already taken
+port_taken ()
+{
+    if netstat -natp | grep docker-proxy | grep "$1" > /dev/null; then
+        echo "The port $1 is already being used by other container!"
+        exit 1
+    fi
 }
 
 ### MAIN
@@ -226,7 +234,7 @@ while [[ $# -gt 0 ]]; do
             ADMINER_LOCAL_PORT="$2"
             shift
         ;;
-        
+
         --port)
             DATABASE_LOCAL_PORT="$2"
             shift 
@@ -270,7 +278,10 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Run all the neccessary checks
-run_checks
+general_checks
+
+# Get IP
+get_ip
 
 # Run adminer?
 if [ "$USE_ADMINER" -eq 1 ]; then
@@ -285,13 +296,11 @@ if [ "$USE_ADMINER" -eq 1 ]; then
     fi
 
     # Give user the info
-    echo "You should be able to connect to adminer by going to http://127.0.0.1:$ADMINER_LOCAL_PORT"
+    echo -e "You should be able to connect to adminer by going to http://127.0.0.1:$ADMINER_LOCAL_PORT \n"
 fi
 
-# DB CT already existing?
-if is_running "$CT_NAME"; then
-    exit 1
-fi
+# Run DB checks
+db_checks
 
 # Starting the container
 if ! docker run -d --name "$CT_NAME" -p "$DATABASE_LOCAL_PORT":3306 -e MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASS" -v "$DB_LOCAL_DATADIR":/var/lib/mysql "$MARIADB_DOCKER_IMG" > /dev/null; then
