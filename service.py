01b7f2080c0e1cdf2dafcdd7c77ad2da59383a1d
Amanda Štramanda <oj@oj.localdomain>
2019-03-12 22:55:26 +0100

    Trying to make a progress...

--- service.py
+++ service.py
@@ -1,19 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
-# TODO: Parsing arguments & Configuration
+# TODO: Parsing CLI arguments & Configuration
+# TODO: Tune the appropriate systemd unit
+# TODO: Spotify authorization
+# TODO: Drop privileges after creating the socket
 
 import sys
 import socket
 import logging
 import time
+import spotipy
+import spotipy.util as util
 import os
 
-# Set the level of logging
-logging.basicConfig(level="INFO")
-
 class Raspotify:
-    
-    def __init__(self):
+   
+    def __init__(self, username, device_id, playlist=None):
         """ Constructor """
 
         # Allowed options
@@ -23,10 +25,35 @@ class Raspotify:
         self.socket = '/var/run/raspotify.sock'
 
         # Spotify playlist uri
-        self.playlist_uri = ""
+        self.playlist_uri = playlist
 
         # Buffer size
         self.buffer_size = 2
+
+        # Spotify username to log in
+        self.spotify_api_username = username
+
+        # Spotify device ID to play on
+        self.spotify_device_id = device_id
+
+        # Max length of received command
+        self.max_command_length = 10
+
+        # Are we authenticated?
+        self.authenticated = False
+
+    # Authenticate to spotify webAPI
+    def authenticate(self):
+
+        try:
+            token = util.prompt_for_user_token(self.spotify_api_username, "user-modify-playback-state", redirect_uri='http://localhost/')
+        except spotipy.SpotifyException as e:
+            logging.error(e)
+            sys.exit()
+
+        if token:
+            sp = spotipy.Spotify(auth=token)
+            self.authenticated = True
 
     def play_next_song(self):
         pass
@@ -69,14 +96,14 @@ class Raspotify:
             command = command.lower()
         except Exception as e:
             print(e)
-            
+
         # Is it an allowed command?
         if command in self.allowed_opts:
 
             # Run the command
             self.run_command(command)
         else:
-            print("Unknown command: " + command)
+            logging.info("Unknown command: " + command)
 
     def run_command(self, command):
         """ Run the specific command """
@@ -105,28 +132,37 @@ class Raspotify:
         # Start listening
         socket.listen(1)
 
+        # Authenticate
+        self.authenticate()
+
         # Infinite loop
         while True:
-            
+
             # (Re)initialize command variable
             command = ''
-            
+
             # Wait for a connection
             logging.debug('Waiting for a connection...')
             connection, client_address = socket.accept()
-            
+
             try:
                 logging.debug('Connection from: ' + client_address)
 
-                # Receive the data in small chunks and retransmit it
+                # Receive the data in chunks
                 while True:
                     data = connection.recv(self.buffer_size)
 
                     # Build the whole message
                     if data:
-                        command += str(data.decode("utf-8"))  
+                        command += str(data.decode("utf-8"))
+
+                        # If someone spams us, send him to hell.
+                        if len(command) > self.max_command_length:
+                            logging.error("Command too long!")
+                            break
                     else:
                         # End of the data stream
+                        logging.debug("Received command is:" + command)
                         self.process_data(command)
                         break
             finally:
@@ -134,8 +170,16 @@ class Raspotify:
                 logging.debug("Closing the connection!")
                 connection.close()
 
-# Create the object
-mainObj = Raspotify()
+# Main
+if __name__ == "__main__":
 
-# Run the shit!
-mainObj.run()
+    # Load the configuration
+
+    # Set the level of logging
+    logging.basicConfig(level="INFO")
+
+    # Create the object
+    mainObj = Raspotify("11131855676", "cfa3f1960a626bef1caeb6c2e3338db25f6d8944", "spotify:user:spotify:playlist:37i9dQZF1DX6ziVCJnEm59")
+
+    # Run the shit!
+    mainObj.run()
